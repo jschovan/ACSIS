fb7bd48a7627a9962f169a4a954022835a41562b
Jaroslava Schovancova <jschovan@gmail.com>
2014-12-07 21:15:23 +0100

    Update host and hostgroup info.

--- acsis/collectors/bin/update_hostgroup_info.sh
+++ acsis/collectors/bin/update_hostgroup_info.sh
@@ -4,11 +4,11 @@
 # Runs on aiadm.cern.ch.
 # @Author: Jaroslava Schovancova <jaroslava.schovancova@cern.ch>
 # 
-URL_HOSTGROUP_UPDATE="http://aipanda022.cern.ch/acsis/hostgroups/"
+URL_HOSTGROUP_UPDATE="http://aipanda022.cern.ch/acsis/hostgroups/api/"
 DIR_BIN="$(dirname $0)"
 DIR_PARENT="${DIR_BIN}/../"
 DIR_OUTPUT="${DIR_PARENT}/output/"
 DATA_FILE="${DIR_OUTPUT}/hostgroup_info_update_raw.json"
 
-curl -s -i -X POST ${URL_HOST_UPDATE} -H "Content-Type: application/json" --data-binary "@${DATA_FILE}"
+curl -s -i -X POST ${URL_HOSTGROUP_UPDATE} -H "Content-Type: application/json" --data-binary "@${DATA_FILE}"
 
